fe910ef09b65b0709ac9ef56258c858c6585323f
Darion Hernandez <darionhernandez868@gmail.com>
2018-04-23 21:21:34 -0400

    Added toast to login form

--- src/app/modules/authentication/components/login-form/login-form.component.ts
+++ src/app/modules/authentication/components/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from './../../../../services/authentication/authentication.service';
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-login-form',
@@ -18,7 +19,8 @@ export class LoginFormComponent {
 
     constructor(private auth: AuthenticationService,
         private formBuilder: FormBuilder,
-        private router: Router) {
+        private router: Router,
+        private toastr: ToastrService) {
         this.loginForm = formBuilder.group({
             'email': [null, Validators.compose([Validators.required, Validators.email])],
             'password': [null, Validators.compose([Validators.required, Validators.minLength(7)])]
@@ -34,15 +36,15 @@ export class LoginFormComponent {
                 isAuthorized.take(1).subscribe(auth => {
                     console.log(auth);
                     if (auth) {
-                        this.router.navigate(['bursary', 'dashboard']);
+                        this.router.navigate(['bursary', 'accounts']);
                     } else {
-                        this.auth.logout().then(() => this.displayError('Unauthorized account'));
+                        this.auth.logout().then(() => this.showErrorToast('Unauthorized account'));
                     }
                 }, error => {
-                    this.displayError('Something went wrong');
+                    this.showErrorToast('Something went wrong');
                 });
             } else {
-                this.displayError('Something went wrong');
+                this.showErrorToast('Something went wrong');
             }
         }).catch(error => {
             this.showError(error);
@@ -53,26 +55,25 @@ export class LoginFormComponent {
         // Error codes at https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signInWithEmailAndPassword
         switch (error.code) {
             case 'auth/user-not-found':
-                this.displayError('User not found');
+                this.showErrorToast('User not found');
                 this.loginForm.reset(); // Clear the contents in the form
                 break;
             case 'auth/wrong-password':
-                this.displayError('Wrong password');
+                this.showErrorToast('Wrong password');
                 this.loginForm.controls['password'].reset(); // Clears only the password field
                 break;
             case 'auth/network-request-failed':
-                this.displayError('Network request failed');
+                this.showErrorToast('Network request failed');
                 this.loginForm.reset(); // Clear the contents in the form
                 break;
             default:
-                this.displayError('Something went wrong');
+                this.showErrorToast('Something went wrong');
                 this.loginForm.reset(); // Clear the contents in the form
         }
         console.error(error);
     }
 
-    displayError(error: string) {
-        this.error = error;
-        console.log(error);
+    showErrorToast(message: string, title?: string) {
+        this.toastr.error(message, title);
     }
 }
